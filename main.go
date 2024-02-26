c19dac6f6671f9b7ef98bd7a2e6bfedd1bfaedf0
VictorSCordeiro <victordesouzacordeiro@gmail.com>
2024-02-26 21:31:22 +0100

    step 2

--- main.go
+++ main.go
@@ -1,10 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	// Extract path from the request URL
+	path := r.URL.Path[1:] // Removing the leading '/'
+
+	// Write the requested path to the response
+	fmt.Fprintf(w, path)
+}
 
 func main() {
 	//var name string
 	fmt.Println("Hello World")
 	//fmt.Scan(&name)
+
+	// Create a new ServeMux
+	mux := http.NewServeMux()
+
+	// Attach handleRequest function to the root endpoint
+	mux.HandleFunc("/", handleRequest)
+
+	// Start the HTTP server on port 3333
+	err := http.ListenAndServe(":3333", mux)
+	if err != nil {
+		fmt.Println("Error starting the server:", err)
+	}
 
 }
