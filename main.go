7e38504b5e7f1f80ae1c2c5f7c529bb79bfd0504
VictorSCordeiro <victordesouzacordeiro@gmail.com>
2024-02-28 13:22:42 +0100

    step 5

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"net/http"
+	"strconv"
 )
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +12,19 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path[1:] // Removing the leading '/'
 
 	// Write the requested path to the response
-	fmt.Fprintf(w, path)
+	fmt.Fprint(w, path)
+}
+
+func handlePrimeRequest(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path[len("/prime/"):]
+	x, err := strconv.Atoi(path)
+	if err != nil {
+		http.Error(w, "Invalid input for x", http.StatusBadRequest)
+		return
+	}
+
+	prime := getNthPrime(x)
+	fmt.Fprintf(w, "%d", prime)
 }
 
 func main() {
@@ -24,10 +38,42 @@ func main() {
 	// Attach handleRequest function to the root endpoint
 	mux.HandleFunc("/", handleRequest)
 
+	// Attach handlePrimeRequest function to the /prime/ endpoint
+	mux.HandleFunc("/prime/", handlePrimeRequest)
+
 	// Start the HTTP server on port 3333
 	err := http.ListenAndServe(":3333", mux)
 	if err != nil {
 		fmt.Println("Error starting the server:", err)
 	}
+}
+func getNthPrime(n int) int {
+	if n <= 0 {
+		return 0
+	}
 
+	count := 0
+	num := 2
+	for {
+		if isPrime(num) {
+			count++
+			if count == n {
+				return num
+			}
+		}
+		num++
+	}
+}
+
+func isPrime(num int) bool {
+	if num <= 1 {
+		return false
+	}
+	maxDiv := int(math.Sqrt(float64(num)))
+	for i := 2; i <= maxDiv; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
 }
--- main_test.go
+++ main_test.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"math/rand"
+	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
+	"time"
 )
 
 func TestShowRequestPathSimple(t *testing.T) {
 	// Generate a random path
-	path := "randomicPathforTest999" // random string of your choice
+	path := generateRandomString(10) // random string of your choice
 
 	// Create a request with the generated path
 	request := httptest.NewRequest("GET", "http://127.0.0.1:3333/"+path, nil)
@@ -22,4 +26,35 @@ func TestShowRequestPathSimple(t *testing.T) {
 	if recordResponse.Body.String() != path {
 		t.Error("Test failed with path " + path)
 	}
+}
+
+// Test case for the /prime/xxx endpoint
+func TestGetNthPrime(t *testing.T) {
+	expectedPrime := 61
+	req := httptest.NewRequest("GET", "http://127.0.0.1:3333/prime/18", nil)
+	rec := httptest.NewRecorder()
+
+	mainHandler := http.HandlerFunc(handlePrimeRequest)
+	mainHandler.ServeHTTP(rec, req)
+
+	if rec.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, rec.Code)
+	}
+
+	body := rec.Body.String()
+	if body != strconv.Itoa(expectedPrime) {
+		t.Errorf("Expected response body %d, but got %s", expectedPrime, body)
+	}
+}
+
+func generateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	randomPath := make([]byte, length)
+
+	rand.Seed(time.Now().UnixNano())
+	for i := range randomPath {
+		randomPath[i] = charset[rand.Intn(len(charset))]
+	}
+
+	return string(randomPath)
 }
